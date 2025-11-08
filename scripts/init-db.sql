881a2413c18e3af7f12457fe9c8ad4020b0cecb8
Owais <mumtaz.owaismeer@gmail.com>
2025-11-08 19:17:41 +0000

    Reorganize scripts and add Airbyte with full data protection

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -15,6 +15,10 @@ GRANT ALL PRIVILEGES ON DATABASE metabase TO admin;
 CREATE DATABASE prefect;
 GRANT ALL PRIVILEGES ON DATABASE prefect TO admin;
 
+-- Create Airbyte data integration database
+CREATE DATABASE airbyte;
+GRANT ALL PRIVILEGES ON DATABASE airbyte TO admin;
+
 -- Connect to main analytics database
 \c analytics;
 
@@ -45,4 +49,5 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA data TO admin;
 \echo '  - n8n (workflow automation)';
 \echo '  - metabase (analytics & BI)';
 \echo '  - prefect (workflow orchestration)';
+\echo '  - airbyte (data integration)';
 \echo '========================================';
--- scripts/start.bat
+++ scripts/start.bat
@@ -1,11 +1,13 @@
 @echo off
 echo Starting Self-Hosted Analytics Stack...
-docker-compose up -d
+docker-compose -f ..\docker-compose.yml up -d
 echo.
 echo Services started!
 echo.
 echo n8n:       http://localhost:5678
 echo Metabase:  http://localhost:3000
+echo Prefect:   http://localhost:4200
+echo Airbyte:   http://localhost:8000
 echo PostgreSQL: localhost:5432 (user: admin)
 echo.
 pause
